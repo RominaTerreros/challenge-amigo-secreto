d6df8aae9c0dd637bdab2703b313bdeeb3dd8173
RominaTerreros <rommina.terreros@unmsm.edu.pe>
2025-08-24 23:52:48 -0500

    modificacion js y html

--- app.js
+++ app.js
@@ -1,1 +1,25 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+let amigos = [];
+
+function asignarTextoElemento(elemento, texto){
+    let elementoHTML = document.querySelector(elemento);
+    elementoHTML.innerHTML = texto;
+    return;
+}
+
+function agregarAmigo() {
+    let amigo = document.getElementById('amigo').value;
+    if (amigo !== "") {
+        amigos.push(amigo);
+        console.log(amigos);
+        limpiarCaja();
+        asignarTextoElemento('li',amigo);
+    } else {
+        alert("Por favor, inserte un nombre.");
+    }
+}
+
+function limpiarCaja() {
+   document.querySelector('#amigo').value = '';
+}
