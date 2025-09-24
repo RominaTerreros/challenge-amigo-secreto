d5a2b19b937f9ec49b497c46a4b76726405cf193
RominaTerreros <101957946+RominaTerreros@users.noreply.github.com>
2025-09-23 21:53:40 -0500

    Update a app.js

--- app.js
+++ app.js
@@ -9,16 +9,14 @@ function asignarTextoElemento(elemento, texto){
 }
 
 function mostrarListaAmigos() {
-    //Obtener el elemento de la lista: Utilizar document.getElementById() o document.querySelector() para seleccionar la lista donde se mostrarán los amigos.
+    //Obtener el elemento de la lista
     let lista = document.getElementById('listaAmigos');
 
-    //Limpiar la lista existente: Establecer lista.innerHTML = "" para asegurarse de que no haya duplicados al actualizar.
-    lista.innerHTML = "";
+    //Limpiar la lista 
 
-//Iterar sobre el arreglo: Usa un bucle for para recorrer el arreglo amigos y crear elementos de lista (<li>) para cada título.
     for (let i = 0; i < amigos.length; i++) {
         let amigo = amigos[i];
-        //Agregar elementos a la lista: Para cada amigo, crear un nuevo elemento de lista.
+        //Agregar elementos a la lista
         lista.innerHTML += `<li>${amigo}</li>`;
     }
 
@@ -42,18 +40,15 @@ function limpiarCaja() {
 }
 
 function sortearAmigo(){
-    //Validar que haya amigos disponibles: Antes de sortear, comprobar si el array amigos no está vacío.
+    //Validar que haya amigos disponibles
     if (amigos.length === 0) {
         alert("No hay amigos para sortear.");
         return;
     }
 
-    //Generar un índice aleatorio: Usar Math.random() y Math.floor() para seleccionar un índice aleatorio del arreglo.
     let indiceAleatorio = Math.floor(Math.random() * amigos.length);
 
-    //Obtener el nombre sorteado: Utilizar el índice aleatorio para acceder al nombre correspondiente en el arreglo.
+    //Obtener el nombre sorteado
     let amigoSorteado = amigos[indiceAleatorio];
-
-    //Mostrar el resultado: Actualizar el contenido del elemento de resultado utilizando document.getElementById()  e innerHTML para mostrar el amigo sorteado.
     document.getElementById('resultado').innerHTML = `El amigo sorteado es: ${amigoSorteado}`;
 }
